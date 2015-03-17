cfbee8cf3006b2e7e94b3f64b1a18a411456ad4a
Li Yichang <yichang.li@autodesk.com>
2015-03-17 17:16:54 +0800

    add search bubble

---
+++ js/headerIcons.js
@@ -0,0 +1,40 @@
+var isCollapsed;
+
+document.getElementById('iconCaretUp').onclick = function(){
+	var settingIcon = document.getElementById('iconSetting');
+	var searchIcon = document.getElementById('iconSearch');
+	var pallet = document.getElementById('pallet');
+	hideShow(settingIcon);
+	hideShow(searchIcon);
+	collapseExpand(pallet);
+
+};
+document.getElementById('iconSearch').onclick = function(){
+	var searchBubble = document.getElementById('searchBubble');
+	if(isCollapsed==true){
+		searchBubble.style.display = 'none';
+	}
+	hideShow(searchBubble);
+
+}
+
+
+function hideShow(icon){
+	if(icon.style.display == 'none'){
+		icon.style.display ='inline';
+	}else{
+		icon.style.display ='none';
+	}
+}
+function collapseExpand(pallet){
+	if(pallet.style.height=='25px'){
+		//expand the pallet
+		pallet.style.height ='306px';
+		isCollapsed = false;
+	}else{
+		//collapse the pallet
+		pallet.style.height = '25px';
+		isCollapsed = true;
+	}
+
+}
