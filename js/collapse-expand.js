07656ce04815dbc7f906d605b86be77b2c953b11
YICHANG <yichang.li@autodesk.com>
2015-03-17 08:12:26 +0800

    hide/show icons while collapse/expand

--- js/collapse-expand.js
+++ js/collapse-expand.js
@@ -1,8 +1,22 @@
-var caretUpIcon = document.getElementById('iconCaretUp'),
-	settingIcon = document.getElementById('iconSetting'),
-	searchIcon = document.getElementById('iconSearch');
+var caretUpIcon = document.getElementById('iconCaretUp');
+	//pallet = document.getElementByClassName('pallet');
 
 caretUpIcon.onclick = function(){
+	if(document.getElementById('iconSetting').style.display=='none'){
+		document.getElementById('iconSetting').style.display='inline';
+		document.getElementById('iconSearch').style.display='inline';
+	}else{
+		document.getElementById('iconSetting').style.display='none';
+		document.getElementById('iconSearch').style.display='none';
+	}
+	
+};
 
-
-}
+// function hideIcon(icon){
+// 	if(icon.style.display=='none'){
+// 		icon.style.display=='block';
+// 		console.log("searchIcon"+searchIcon.style.display);
+// 	}else{
+// 		icon.style.display=='none';
+// 	}
+// }
