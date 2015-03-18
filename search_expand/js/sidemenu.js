d93859cc60682a30c1f2dd166a59c91d1bc08a4d
Li Yichang <yichang.li@autodesk.com>
2015-03-18 15:51:12 +0800

    new search approach
    search bar expanded from search icon

---
+++ search_expand/js/sidemenu.js
@@ -0,0 +1,8 @@
+var hamburgerIcon = document.getElementById( 'iconHamburger' ),
+	body = document.querySelector('div.side-menu-push');
+
+			
+hamburgerIcon.onclick = function() {
+	classie.toggle( this, 'active' );
+	classie.toggle( body, 'side-menu-push-toleft' );
+};
