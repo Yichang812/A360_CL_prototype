1880e0c2793dfb2ec872b18fba3bcaaddb9f74d6
Li Yichang <yichang.li@autodesk.com>
2015-04-23 12:44:12 +0800

    beautify the ui based on design guideline

--- full-function/js/index.js
+++ full-function/js/index.js
@@ -1,21 +1,36 @@
-// the search bar slide down or collapse up while clicking search icon
 $(document).ready(function(){
 
-	//the search bar slide left or back while clicking search icon
+//the search bar slide left or back while clicking search icon
 	$('#searchIcon , #closeIcon').click(function(){
-		if($('.search-bar').position().left > 0) {
-			$('.search-bar').animate({left:'0',backgroundColor:'#d3e5f2'},200);
-			$('#searchIcon , #closeIcon').animate({color:'#217cbc'},200);
+		//if search bar is not visible, show it
+		if($('.search-bar').position().left !== 0) {
+			$('.search-bar').animate({left:'0',backgroundColor:'#116fb8'},200);
+			$('#searchIcon , #closeIcon').animate({color:'white'},200);
 		}else{
 			$('.search-bar').animate({left:'181',backgroundColor:'white'},200);
 			$('#searchIcon , #closeIcon').animate({color:'#333'},200);
 		}
 	});
 
+	$('.search-bar').hover(
+		//the function works only when the search bar is not visible
+		function() {
+			if($('.search-bar').position().left !== 0){
+				$(this).css('backgroundColor', '#116fb8');
+				$('#searchIcon').css('color', 'white');
+			}
+		},
+		function(){
+			if($('.search-bar').position().left !== 0){
+				$(this).css('backgroundColor', 'white');
+				$('#searchIcon').css('color', '#333');
+			}
+		});
+
 
 	// the side menu slide down or back while clicking hamburger menu icon
 	// the toggle function used here is from 'jquery ui' library
-$('#menuIcon').click(function(){
+	$('#menuIcon').click(function(){
 		$('.side-menu').toggle('slide',{direction:'up'},200,sidemenuColor);
 	});
 
@@ -26,27 +41,23 @@ $('#menuIcon').click(function(){
 		$('.side-menu').toggle('slide',{direction:'up'},200,sidemenuColor);
 	});
 
+	//if the side menu is shown, set the background color to be darker and the subheader to be blue
+	var sidemenuColor = function(){
+		if($('.side-menu').is(':visible')){
+			$('.shadow').show(150,function(){
+				$(this).animate({backgroundColor:'rgba(0,0,0,0.2)'})
+			});
+			$('.search-bar').hide();
+			$('.subheader').animate({backgroundColor:'#116fb8',zIndex:'2'});
+			$('.current-view,#menuIcon').animate({color:'white'});
+
+		}else{
+			$('.subheader').animate({backgroundColor:'white',zIndex:''});
+			$('.current-view,#menuIcon').animate({color:'#333'});
+			$('.shadow').hide();
+			$('.search-bar').show();
+		}
+	};
 });
 
-//if the side menu is shown, set the background color to be darker and the subheader to be blue
-var sidemenuColor = function(){
-	if($('.side-menu').is(':visible')){
-		$('.shadow').show(150,function(){
-			$(this).animate({backgroundColor:'rgba(0,0,0,0.2)'})
-		});
-		$('.subheader').animate({backgroundColor:'#d3e5f2'});
-	}else{
-		$('.subheader').animate({backgroundColor:'white'});
-		$('.shadow').hide();
-	}
-};
 
-
-//hightlight current view in the side menu
-
-// var category = $('nav').children('a,div');
-// for(var i = 0; i<category.length ; i++){
-// 	if (category[i].text()==$('.current-view').text()){
-// 		category[i].css('backgroundColor':'#d3e5f2');
-// 	}
-// }
